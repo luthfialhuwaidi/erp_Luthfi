f6ea592e4442450c907b4d7ca20170dbe6d6c04d
alhuwyd pii <luthfialhuwaidipelalawan@gmail.com>
2026-09-09 14:42:49 +0700

    Initial commit project erp

---
+++ app/Filament/Resources/Companies/Tables/CompaniesTable.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Resources\Companies\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CompaniesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('logo')
+                    ->disk('public'), // Hapus ->searchable() di sini
+
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('address')
+                    ->searchable(),
+
+                TextColumn::make('email')
+                    ->label('Email address')
+                    ->searchable(),
+
+                TextColumn::make('phone_number')
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    // DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
